78c85640884d733ba424e4cdee6221fdd6f42509
SJH <shenjunhong@meizu.com>
2020-04-19 19:42:39 +0800

    useContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,83 +1,42 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, createContext,useContext } from "react";
 import "./App.css";
 import { Component } from "react";
 
-class App extends Component {
-  state = {
-    count: 0,
-    size: {
-      width: 0,
-      height: 0
-    }
-  };
+const CountContext = createContext();
 
-  onResize = () => {
-    this.setState({
-      size: {
-        width: document.documentElement.clientWidth,
-        height: document.documentElement.clientHeight
+class Foo extends Component{
+  render(){
+    return(
+      <CountContext.Consumer>
+      {
+        count => <h1>{count}</h1>
       }
-    });
-  };
-  componentDidMount() {
-    document.title = this.state.count;
-    window.addEventListener("resize", this.onResize, false);
+      </CountContext.Consumer>
+    )
   }
+}
 
-  render() {
-    const { count, size } = this.state;
-    return (
-      <button
-        onClick={() => {
-          this.setState({ count: this.state.count + 1 });
-        }}
-      >
-        Click({count}) size:({size.width}, {size.height})
-      </button>
-    );
+function Counter(){
+  const count = useContext(CountContext)
+  return(
+    <h1>{count}</h1>
+
+  ) 
+}
+
+class Bar extends Component{
+  static contextType = CountContext;
+  render(){
+    const count = this.context;
+    return(
+      <h1>{count}</h1>
+    )
   }
 }
 
 function App2(props) {
   const [count, setCount] = useState(0);
-  const [size, setSize] = useState({
-    width: document.documentElement.clientWidth,
-    height: document.documentElement.clientHeight
-  });
-  //useState 按照第一次返回的顺序给你state的
 
-  useEffect(() => {
-    document.title = count;
-  });
-
-  useEffect(() => {
-    console.log("count", count);
-  }, [count]);
-
-  useEffect(() => {
-    window.addEventListener("resize", onResize, false);
-    return () => {
-      window.removeEventListener("resize", onResize, false);
-    };
-  }, []);
-
-  useEffect(() => {
-    document.querySelector('#size').addEventListener("click", onClick, false);
-/*     return () => {
-      document.querySelector('#size').removeEventListener("click", onClick, false);
-    }; */
-  });
-
-  const onClick = () => {
-    console.log("click");
-  };
-
-  const onResize = () => {
-    setSize({
-      width: document.documentElement.clientWidth,
-      height: document.documentElement.clientHeight
-    });
-  };
   return (
     <div>
       <button
@@ -87,15 +46,12 @@ function App2(props) {
       >
         Click({count})
       </button>
-      {count % 2 ? (
-        <span id="size">
-          size:({size.width}, {size.height})
-        </span>
-      ) : (
-        <p id="size">
-          size:({size.width}, {size.height})
-        </p>
-      )}
+      <CountContext.Provider value={count}>
+        <Foo/>
+        <Bar/>
+        <Counter/>
+      </CountContext.Provider >
+
     </div>
   );
 }
