1f7c8bf21198c3d19582336bc92b535d15483770
SJH <shenjunhong@meizu.com>
2020-05-19 19:28:37 +0800

    todolist

--- src/App.jsx
+++ src/App.jsx
@@ -1,79 +1,41 @@
 import React, { PureComponent,useState, useEffect,createContext,useContext, useMemo, memo , useCallback, useRef} from "react";
 import "./App.css";
 
-function useCounter(props){
-  const size = useSize()
-  return(
-  <h1 >{props}, {size.width} * {size.height}</h1>
+function List(props){
+  const { list } = props
+
+  return (
+    <ul>
+      {
+        list.map(item=>{
+          return (
+            <li>
+            <input type="checkout"/>
+            <span>item.val</span>
+            <span>x</span>
+            </li>
+          )
+        })
+      }
+    </ul>
   )
 }
 
-
-
-function useCount(defaultCount) {
-  const [count, setCount] = useState(defaultCount);
-  const it = useRef();
-
-
-  useEffect(()=>{
-    it.current = setInterval(()=>{
-      setCount(count => count +1)
-    }, 1000)
-  }, [])
-
-  useEffect(()=>{
-    if(count >= 10){
-      clearInterval(it.current)
-    }
-  })
-  return  [count, setCount]
-};
-
-function useSize(){
-
-  function onResize(){
-    setSize({
-        width: document.documentElement.clientWidth,
-        height: document.documentElement.clientHeight,
-      }
-    )
-  }
-  const [size, setSize] = useState({
-    width: document.documentElement.clientWidth,
-    height: document.documentElement.clientHeight,
-  })
-
-  useEffect(()=>{
-    window.addEventListener('resize', onResize, false)
-    return ()=>{
-    window.removeEventListener('resize', onResize, false)
-
-    }
-  }, [])
-
-  return size
-}
-
-
-
-
-function App2(props) {
-const [count, setCount] = useCount(0);
-const Counter = useCounter(count)
-const size = useSize();
+function Todo(){
   return (
     <div>
-      <button
-        onClick={() => {
-          setCount(count + 1);
-        }}
-      >
-        Click({count})
-      </button>
-      {size.width} * {size.height}
-      {Counter}
+      <h1>todo</h1> 
+      <input type="text" placeholder='why we need to do' />
     </div>
+  )
+}
+
+function App(props) {
+  const [todo, setTodo] = useState([]);
+
+  return (
+    <Todo></Todo>
   );
 }
 
-export default App2;
+export default App;
