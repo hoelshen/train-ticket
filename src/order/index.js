8bcdb66a99963a5b08617d368a32c449a0495069
SJH <shenjunhong@meizu.com>
2020-06-04 20:57:04 +0800

    添加mpa

---
+++ src/order/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import ReactDOM  from 'react-dom'
+
+import { Provider } from 'react-redux';
+
+import store from './store'
+
+
+import 'normalize.css/normalize.css';
+
+import './index.css';
+
+import App from './App.jsx';
+
+ReactDOM.render(
+  <Provider store={store}> 
+    <App/>
+  </Provider>
+, document.getElementById('root'))
---
+++ src/ticket/App.jsx
@@ -0,0 +1,13 @@
+import { connect } from "react-redux";
+
+import './App.css';
+function App(){
+
+
+
+}
+
+export default connect(
+  function mapStateToProps(state) {},
+  function mapDispatchToProps(dispatch) {}
+)(App);
